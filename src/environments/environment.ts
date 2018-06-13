63fd58391c6eeba345e9b51dbb70d985e5ffdd99
Robert Jenifer Vilbert <robertarjun46@gmail.com>
2018-06-13 21:56:06 +0530

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAAJLf2dzc9DqQuda7WPerZ_vOP6BYBJ1k',
-    authDomain: 'angular-chat-fca80.firebaseapp.com',
-    databaseURL: 'https://angular-chat-fca80.firebaseio.com',
-    projectId: 'angular-chat-fca80',
-    storageBucket: 'angular-chat-fca80.appspot.com',
-    messagingSenderId: '65609771970'
+    apiKey: 'your API Key',
+    authDomain: 'Your Domain name',
+    databaseURL: 'db url',
+    projectId: 'your project id',
+    storageBucket: 'storage',
+    messagingSenderId: 'id'
   }
 };
